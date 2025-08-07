b044c81c23f2e6d234d5c27cbd4012dde396c272
appleboblin <github@appleboblin.com>
2025-08-08 00:33:37 +0800

    Update art.c

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -385,7 +385,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_FACE1
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 const lv_img_dsc_t face1 = {
-    .header.cf = LV_IMG_CF_ALPHA_1BIT,
+    .header.cf = LV_IMG_CF_INDEXED_1BIT,
     .header.always_zero = 0,
     .header.reserved = 0,
     .header.w = 140,
